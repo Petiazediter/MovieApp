074ea184642374b14312f751c485ebf33ed0d044
Peti <49650243+Petiazediter@users.noreply.github.com>
2023-11-29 17:23:12 +0100

    feat: update context with prisma client

--- server/src/definitions/context/index.ts
+++ server/src/definitions/context/index.ts
@@ -1,9 +1,15 @@
 import { ExpressContextFunctionArgument } from "@apollo/server/dist/esm/express4";
+import { PrismaClient } from "@prisma/client";
+
+const PRISMA_CLIENT = new PrismaClient();
 
 export interface Context {
     // Add your context properties here
+    db: PrismaClient
 }
 
 export const resolveContext = async (params: ExpressContextFunctionArgument): Promise<Context> => {
-    return {}
+    return {
+        db: PRISMA_CLIENT
+    }
 }
