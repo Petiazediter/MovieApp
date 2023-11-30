b3644102e755e21bd1b7ee74b875e017c510670f
Peti <49650243+Petiazediter@users.noreply.github.com>
2023-11-30 13:59:36 +0100

    feat: get movies from api

---
+++ server/src/api/getMovies.ts
@@ -0,0 +1,57 @@
+import { Movie } from "../gql/graphql.types";
+import axios from "axios";
+
+interface MovieApiResponse {
+    page: number,
+    total_pages: number,
+    total_results: number,    
+    results: MovieApiResult[]
+}
+
+interface MovieApiResult {
+    adult: boolean,
+    id: number,
+    overview: string,
+    poster_path: string,
+    title: string,
+    release_date: string
+}
+
+interface MoviesData {
+    movies: Movie[],
+    metadata: {
+        totalPages: number,
+        totalResults: number
+    }
+}
+
+export const getMovies = (query: string, page: number): Promise<MoviesData> => {
+    return new Promise((resolve, reject) => {
+        try {
+            axios.get<MovieApiResponse>(`https://api.themoviedb.org/3/search/movie?query=${query.replaceAll(' ', '%20')}&include_adult=false&language=en-US&page=${page}`, {
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: `Bearer ${process.env.MOVIE_DB_READ_ACCESS_TOKEN}`
+                }
+            }).then(response => {
+                resolve({
+                    movies: castMovies(response.data),
+                    metadata:{
+                        totalPages: response.data.total_pages,
+                        totalResults: response.data.total_results
+                    }
+                })
+            })
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+const castMovies = (movies: MovieApiResponse): Movie[] => {
+    return movies.results.map(apiMovie => ({
+        ...apiMovie,
+        description: apiMovie.overview,
+        coverArt: apiMovie.poster_path
+    }))
+}
