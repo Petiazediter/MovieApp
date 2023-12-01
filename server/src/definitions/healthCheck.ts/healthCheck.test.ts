31e1abfd8b3a8a1791cd9a6551b65337713100ae
Péter Verebes <pverebes74@gmail.com>
2023-12-01 14:29:27 +0100

    feat: extend healthCheck test

--- server/src/definitions/healthCheck.ts/healthCheck.test.ts
+++ server/src/definitions/healthCheck.ts/healthCheck.test.ts
@@ -1,5 +1,26 @@
+import { ApolloServer } from "@apollo/server";
+import { getServerEntities } from "../../server";
+import assert from "assert";
+import { Context } from "../context";
+
 describe('Health Check', () => {
-    it('should return true', () => {
-        expect(true).toBe(true);
+    let testServer: ApolloServer<Context>;
+    
+    beforeAll( () => {
+        testServer = getServerEntities().server;
+    })
+
+    it('Health-Check query should return true', async () => {
+        const response = await testServer.executeOperation({
+            query: `
+                query HealthCheck {
+                    healthCheck
+                }
+            `
+        })
+
+        assert(response.body.kind === 'single')
+        expect(response.body.singleResult.errors).toBeUndefined()
+        expect(response.body.singleResult.data?.healthCheck).toBe(true)
     });
 });
