2025628b2433343e201d23cd0376a292a4950556
Péter Verebes <pverebes74@gmail.com>
2023-12-01 14:29:07 +0100

    chore: remove redis from context

--- server/src/definitions/context/index.ts
+++ server/src/definitions/context/index.ts
@@ -1,16 +1,12 @@
 import { ExpressContextFunctionArgument } from "@apollo/server/dist/esm/express4";
 import { PrismaClient } from "@prisma/client";
-import { RedisClient } from "../../cache/redisClient";
 
 const PRISMA_CLIENT = new PrismaClient();
-const REDIS_CLIENT = new RedisClient();
 export interface Context {
     // Add your context properties here
     db: PrismaClient
-    redis: RedisClient
 }
 
 export const resolveContext = async (params: ExpressContextFunctionArgument): Promise<Context> => ({
     db: PRISMA_CLIENT,
-    redis: REDIS_CLIENT
 })
