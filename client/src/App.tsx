70bf56dec77b7d6b10222a930c7f49f333162bc1
Péter Verebes <pverebes74@gmail.com>
2023-12-08 12:26:13 +0100

    fix: fetchPolicy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { gql, useLazyQuery } from '@apollo/client';
 import { FetchType, SearchMovieQuery, SearchMovieQueryArguments } from './gql/movies';
 import SearchBox from './components/SearchBox';
 import MovieList from './components/MovieList';
+import InfoBox from './components/Infobox';
 
 function App() {
 
@@ -21,19 +22,20 @@ function App() {
         }
         totalPages
       } 
-    }`
+    }`, { fetchPolicy: 'network-only' }
   )
 
   const callback = useCallback( (value: string) => {
     fetchData({variables: {
       keyword: value,
       page: 1,
-    }})
+    }, fetchPolicy: 'no-cache'})
   }, [fetchData])
   
   return (
     <main className="App">
         <SearchBox onSubmit={callback} />
+        { data && <InfoBox text={`Results are pulled from ${data.searchMovies.fetchType}`} />}
         <MovieList movieResults={data?.searchMovies ?? {
           movies: [],
           totalPages: 0,
