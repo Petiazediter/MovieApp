1e861da702cf930770bcce271f4b8a416be3d223
Péter Verebes <pverebes74@gmail.com>
2023-12-08 11:59:49 +0100

    feat: utilize lazy query to get movies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { gql, useLazyQuery } from '@apollo/client';
+import { FetchType, SearchMovieQuery, SearchMovieQueryArguments } from './gql/movies';
 
 function App() {
+
+  const [fetchData, { data, error }] = useLazyQuery<SearchMovieQuery, SearchMovieQueryArguments>(gql`
+    query SearchQuery($keyword: String!, $page: Int) {
+      searchMovies(keyword: $keyword, page: $page) {
+        fetchType
+        movies {
+          id   
+          overview
+          backgroundImagePath
+          posterImagePath
+          releaseDate
+          title
+        }
+        totalPages
+      } 
+    }`
+  )
+
+  const callback = useCallback( (value: string) => {
+    fetchData({variables: {
+      keyword: value,
+      page: 1,
+    }})
+  }, [fetchData])
+  
   return (
     <div className="App">
       <header className="App-header">
