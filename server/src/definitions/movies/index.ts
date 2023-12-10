7e4eb162e9ec3948bd8bb2c2981a3d861aaded64
Péter Verebes <pverebes74@gmail.com>
2023-12-10 14:30:31 +0100

    fix: error handle

--- server/src/definitions/movies/index.ts
+++ server/src/definitions/movies/index.ts
@@ -36,90 +36,103 @@ const resolvers: Resolvers = {
     Query: {
         searchMovies: async (_root, { keyword, page }, { db }) => {
             // check if keyword is already cached:
-            const twoMinsAgo = new Date(Date.now() - (2 * 60 * 1000)).toISOString()
-            const cachedKeyword = await db.searchedKeyword.findUnique({
-                where: {
-                    keywordIdentifier: {
-                        page: page ? page : 1,
-                        keyword: toUpper(keyword)
+            try {
+                const twoMinsAgo = new Date(Date.now() - (2 * 60 * 1000)).toISOString()
+                const cachedKeyword = await db.searchedKeyword.findUnique({
+                    where: {
+                        keywordIdentifier: {
+                            page: page ? page : 1,
+                            keyword: toUpper(keyword)
+                        },
+                        updatedAt: {
+                            gte: twoMinsAgo
+                        }
                     },
-                    updatedAt: {
-                        gte: twoMinsAgo
+                    select: {
+                        movies: true,
+                        totalPage: true,
                     }
-                },
-                select: {
-                    movies: true,
-                    totalPage: true,
-                }
-            })
+                })
 
-            if ( !cachedKeyword ) {
-                // if not then get the movies from the third party API
-                const { movies, metadata: { totalPages }} = await getMovies(keyword, page ?? 1)
-                
-                // Save the movies and the keyboard to the db
-                const moviesToSave = getMoviesToSave(movies)
+                if ( !cachedKeyword ) {
+                    // if not then get the movies from the third party API
+                    try {
+                        const { movies, metadata: { totalPages }} = await getMovies(keyword, page ?? 1)
+                    
+                        // Save the movies and the keyboard to the db
+                        const moviesToSave = getMoviesToSave(movies)
 
-                if ( movies.length > 0 ) {
-                    await db.searchedKeyword.upsert({
+                        if ( movies.length > 0 ) {
+                            try {
+                                await db.searchedKeyword.upsert({
+                                    where: {
+                                        keywordIdentifier: {
+                                            keyword: toUpper(keyword),
+                                            page: page ?? 1,
+                                        }
+                                    },
+                                    update: {
+                                        // reset cache counter
+                                        cacheCounter: 0,
+                                        totalPage: totalPages,
+                                        movies: {
+                                            upsert: moviesToSave,
+                                        },
+                                    },
+                                    create: {
+                                        keyword: toUpper(keyword),
+                                        page: page ?? 1,
+                                        cacheCounter: 0,
+                                        totalPage: totalPages,
+                                        movies: {
+                                            connectOrCreate: moviesToSave.map(v => ({
+                                                where: v.where,
+                                                create: v.create
+                                            }))
+                                        }
+                                    }
+                                })
+                            } catch (e) {
+                                throw new Error(`Database error: ${JSON.stringify(e)}`)
+                            }
+                        }
+                        
+                        return {
+                            movies,
+                            totalPages,
+                            fetchType: FetchType.Api
+                        }
+                    }
+                    catch (e) {
+                        throw new Error(`API Error ${JSON.stringify(e)}`)
+                    }
+
+                } else {
+                    // if the keyword is already cached then just return the saved values from the db
+                    
+                    // increment the cache counter
+                    await db.searchedKeyword.update({
                         where: {
                             keywordIdentifier: {
                                 keyword: toUpper(keyword),
                                 page: page ?? 1,
                             }
                         },
-                        update: {
-                            // reset cache counter
-                            cacheCounter: 0,
-                            totalPage: totalPages,
-                            movies: {
-                                upsert: moviesToSave,
-                            },
-                        },
-                        create: {
-                            keyword: toUpper(keyword),
-                            page: page ?? 1,
-                            cacheCounter: 0,
-                            totalPage: totalPages,
-                            movies: {
-                                connectOrCreate: moviesToSave.map(v => ({
-                                    where: v.where,
-                                    create: v.create
-                                }))
+                        data: {
+                            cacheCounter: {
+                                increment: 1,
                             }
                         }
                     })
-                }
-                
-                return {
-                    movies,
-                    totalPages,
-                    fetchType: FetchType.Api
-                }
 
-            } else {
-                // if the keyword is already cached then just return the saved values from the db
-                
-                // increment the cache counter
-                await db.searchedKeyword.update({
-                    where: {
-                        keywordIdentifier: {
-                            keyword: toUpper(keyword),
-                            page: page ?? 1,
-                        }
-                    },
-                    data: {
-                        cacheCounter: {
-                            increment: 1,
-                        }
+                    return {
+                        movies: castToResolverMovies(cachedKeyword.movies),
+                        totalPages: cachedKeyword.totalPage,
+                        fetchType: FetchType.Db
                     }
-                })
-
-                return {
-                    movies: castToResolverMovies(cachedKeyword.movies),
-                    totalPages: cachedKeyword.totalPage,
-                    fetchType: FetchType.Db
                 }
+            } catch (e) {
+                throw new Error(`Database error: ${JSON.stringify(e)}`)
             }
         }
     }
